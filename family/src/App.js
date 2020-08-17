4af179294530c0a3314909429e261464df677e3a
Taima <taimab98@gmail.com>
2020-08-17 16:16:35 +0300

    taima

--- family/src/App.js
+++ family/src/App.js
@@ -4,21 +4,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="main">
+    <div className="Login">
+      <link href="https://fonts.googleapis.com/css2?family=Pangolin&display=swap" rel="stylesheet"></link>
+      <header className="Login-header">
+        Login
       </header>
+      <form className="login-form">
+        <input id="field" className="lastName" placeholder="Last Name" type="text"></input>
+        <input id="field" className="password" placeholder="Password" type="password"></input>
+        <button id="field" onclick="Tasks()">Login</button>
+        <button id="field" onclick="Register()">Register</button>
+      </form>
+    </div>
     </div>
   );
 }
