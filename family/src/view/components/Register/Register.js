c912ba8b14427820ff435893c682e08b33be01a6
Saleh <salehao1996@gmail.com>
2020-08-17 15:46:57 +0300

    register

---
+++ family/src/view/components/Register/Register.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+import './Register.css';
+
+function Login(props) {
+
+    return (
+        <div className='root'>
+            <form className='form'>
+            <h2 class="active"> Register </h2>
+
+                <input type='text' placeholder='Email' name='userEmail'></input>
+                <br />
+                <input type='text' placeholder='First Name' name='firstName'></input>
+                <br />
+                <input type='text' placeholder='Last Name' name='lastName'></input>
+                <br />
+                <input type='text' placeholder='Pic URL' name='imgUrl'></input>
+                <br />
+                <input type='text' placeholder='Password' name='password'></input>
+                <br />
+                <input type='submit' onClick={handleSubmit} value="Register" id='button'></input>
+            </form>
+        </div>
+    );
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        const email = e.target.elements.userEmail.value;
+        const firstname = e.target.elements.firstName.value;
+        const lastname = e.target.elements.lastName.value;
+        const img = e.target.elements.imgUrl.value;
+        const password = e.target.elements.password.value;
+        fetch('/register', {
+            method: 'POST',
+            body: JSON.stringify({ email, firstname, lastname, img, password }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+
+        console.log("cats are here")
+    }
+
+}
+
+
+
+export default Login;
+
