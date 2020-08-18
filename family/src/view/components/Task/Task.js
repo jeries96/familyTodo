2ff9c4caeacf5766afa3b2570c6cd945fcd0fdd6
Saleh <salehao1996@gmail.com>
2020-08-18 15:08:27 +0300

    style

--- family/src/view/components/Task/Task.js
+++ family/src/view/components/Task/Task.js
@@ -1,26 +1,36 @@
 import React, { useState } from 'react';
 
 import './Task.css';
+import checked from './checked.svg'
+import Delete from './delete.svg';
+
 
 function Task(props) {
     const { elem } = props
     console.log(elem)
     return (
-        <div>
+        <div id="wrapper">
 
-
+            <div id="user">{elem._id}</div>
             <div className="taskwrapper">
-                <div id="user">{elem._id}</div>
+                {/* <div id="user">{elem._id}</div> */}
                 <div id="titles">
                     {
                         elem.tasks.map((task) => {
-                            return (<div>{task.taskTitle}</div>)
+                            return (
+                                <div id="flexx">
+                                    <div>{task.taskTitle}</div>
+                                    <div id="option">
+                                        <img src={checked} />
+                                        <img src={Delete} />
+                                    </div>
+                                </div>
+
+                            )
                         })
                     }
                 </div>
-                <div id="option">
 
-                </div>
             </div>
 
 
