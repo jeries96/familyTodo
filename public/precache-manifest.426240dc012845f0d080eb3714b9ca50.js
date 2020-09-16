92db29da97689fe1a4d6277478e45a4e1d84dc4c
jeries <geriesmazzawi@gmail.com>
2020-09-16 19:45:16 +0300

    s

---
+++ public/precache-manifest.426240dc012845f0d080eb3714b9ca50.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4313fd6a2c0238950dbfe8fe3a952395",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18040527018dec7c8fe3",
+    "url": "/static/css/main.bbc00247.chunk.css"
+  },
+  {
+    "revision": "273808b8dff46f4ef78f",
+    "url": "/static/js/2.ff9550ef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ff9550ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18040527018dec7c8fe3",
+    "url": "/static/js/main.bcc64616.chunk.js"
+  },
+  {
+    "revision": "2272923afc6c5ac703f7",
+    "url": "/static/js/runtime-main.823863db.js"
+  },
+  {
+    "revision": "b7fe0c5272c70450cc5e085647b95cb0",
+    "url": "/static/media/checked.b7fe0c52.svg"
+  },
+  {
+    "revision": "1e2a80a4622dd8b46b2aff5bc3c7b357",
+    "url": "/static/media/delete.1e2a80a4.svg"
+  },
+  {
+    "revision": "9d3d7d1210749c52e742047f7a503dc1",
+    "url": "/static/media/greyBackground.9d3d7d12.jpg"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a23f40418996674c61dc7592d654beac.js"
+  "/precache-manifest.426240dc012845f0d080eb3714b9ca50.js"
 );
 
 self.addEventListener('message', (event) => {
